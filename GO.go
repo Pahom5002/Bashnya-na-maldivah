5e4521adf802e43bfaad9f61d6a7d1ecbc0c9f7f
Pahom5002 <pashka.kiryanov5002@mail.ru>
2025-10-19 03:03:16 +0300

    Add files via upload

--- GO.go
+++ GO.go
@@ -3,6 +3,53 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	var num float64
+	fmt.Println("введите число меньше 12307")
+
+begin:
+
+	fmt.Scan(&num)
+	num = num
+
+	if num > 12307 {
+		fmt.Println("вы ввели чило болше 12307, введите число меньше этого числа, пожалуйста;)")
+		goto begin
+	}
+	fmt.Println("итерации в цикле:")
+	for num < 12307 {
+		if int(num)%13 == 0 {
+			if int(num)%9 == 0 {
+				fmt.Println("servise error")
+			}
+		}
+		if int(num)%13 != 0 {
+			if int(num)%9 != 0 {
+				num = num + 1
+			}
+		}
+
+		if num < 0 {
+
+			num = num * -1
+
+		}
+		if int(num)%7 == 0 {
+
+			num = num * 39
+
+		}
+		if int(num)%9 == 0 {
+
+			num = num*13 + 1
+
+		} else {
+			num = (num + 2) * 3
+
+		}
+		fmt.Println(num)
+
+	}
+	fmt.Println("________________________")
+	fmt.Println("Итоговое значение:", num)
 
 }
